26efc906d7fc0dc81366ed42f2cca2c20ee551b6
shyguyCreate <107062289+shyguyCreate@users.noreply.github.com>
2023-11-27 08:43:55 -0600

    feat: add nginx container

--- app/app.py
+++ app/app.py
@@ -2,17 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Anadir a los miembros manualmente
-member1 = "Integrante 1"
-member2 = "Integrante 2"
-member3 = "Integrante 3"
-
 
 @app.route("/")
 def hello_world():
-    return render_template(
-        "index.html", member1=member1, member2=member2, member3=member3
-    )
+    return render_template("index.html")
 
 
 # Run app
